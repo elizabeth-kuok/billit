8c97119261b99c45daac15db965df75478d3e388
elizaebth <elizabethkuok29@gmail.com>
2019-03-29 02:38:09 -0600

    redirect on signup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -41,6 +41,7 @@ export class AuthService {
     this.afAuth.auth.createUserWithEmailAndPassword(
       authData.email, authData.password
     ).then(result => {
+      this.router.navigate(['bills']);
       console.log(result);
     }).catch(err => {
       console.log(err);
